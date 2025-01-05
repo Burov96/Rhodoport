4fa70b7181dd06752fe627fdb261c824d71fe2c8
Teodor <burov96@gmail.com>
2025-01-05 15:34:47 +0200

    Fix API calls and database connection issues

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  // host: process.env.NODE_ENV === 'production' ? 'rnksg-94-156-23-153.a.free.pinggy.link' : 'localhost',
   host:process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '45593'),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'mangasarkon',
-  database: process.env.DB_NAME || 'city_directory',
+  port: parseInt(process.env.DB_PORT || '16450', 10),
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
--- testche.js
+++ testche.js
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise';
 (async () => {
   try {
     const connection = await mysql.createConnection({
-      host: 'rnksg-94-156-23-153.a.free.pinggy.link',
-      port: 45593,
+      host: 'rngfw-94-156-23-153.a.free.pinggy.link',
+      port: 16450,
       user: 'root',
       password: 'mangasarkon',
       database: 'city_directory',
